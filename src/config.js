ba24bdd3d8c8c38bcf7f9f33f9b59108308f1c1a
parvej236 <alamparvej2024@gmail.com>
2026-04-14 18:37:02 +0600

    Updated for deployment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = 'http://localhost:8080'
-
+export const BACKEND_URL = import.meta.env.VITE_API_URL;
+console.log('Using API URL:', BACKEND_URL)
 export const API_URL = `${BACKEND_URL}/api`
